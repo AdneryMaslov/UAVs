0dcdb8d6b95fd43118bafe15af5641412e344903
KarmaMinus7x5 <100802429+KarmaMinus7x5@users.noreply.github.com>
2024-12-10 19:27:53 +0500

    Вывод имени файла
    Вывод имени файла

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ async def upload_file(file: UploadFile = File(...)):
     if file.filename.endswith('.zip'):
         result_zip_path = await process_zip(file)
         if result_zip_path and os.path.exists(result_zip_path):
-            return FileResponse(result_zip_path)
+            return {"filename": result_zip_path.name}
         else:
             raise HTTPException(status_code=200, detail="На изображении в архиве ничего не найдено")
     else:
@@ -39,7 +39,7 @@ async def upload_file(file: UploadFile = File(...)):
         processed_image_path = process_image(file_path)
 
         if processed_image_path and os.path.exists(processed_image_path):
-            return FileResponse(processed_image_path)
+            return {"filename": processed_image_path.name}
         else:
             raise HTTPException(status_code=200, detail="На изображении ничего не найдено")
 
